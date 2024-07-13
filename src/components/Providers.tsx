7a1de8bad4c61478c438891c621860ac22e950c4
Sam Barot <sam29061994@gmail.com>
2024-07-13 15:58:20 -0600

    Fix - Updated trpc httpBatchLink to production url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -10,7 +10,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3000/api/trpc',
+          url: 'https://quaser.vercel.app/api/trpc',
         }),
       ],
     })
